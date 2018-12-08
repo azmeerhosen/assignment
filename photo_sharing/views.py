3d4c57bfb8ebcf4006aaeb1f1ca4e7c078da1cc5
Azmeer <azmeer.ruet12@gmail.com>
2018-12-08 23:48:31 +0600

    trying saving uploaded photo info to db

--- photo_sharing/views.py
+++ photo_sharing/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from helper import handle_upload
+from photo_sharing.models import Photos
 
 
 def home_view(request):
@@ -14,7 +15,9 @@ def home_view(request):
 
 def upload_view(request):
     if request.method == 'POST' and request.FILES['photo']:
-        handle_upload(request.FILES['photo'], str(request.FILES['photo']))
+        file_name = 'azmeer' + str(request.FILES['photo'])
+        handle_upload(request.FILES['photo'], file_name)
+        Photos.objects.create(username=request.user, photo_location=file_name)
         return HttpResponse("Successful")
     return render(request, 'upload_file.html', context={})
 
